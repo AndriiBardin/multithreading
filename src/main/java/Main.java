959088dac1f418338a79a1a2a351c969db8abe38
Andrii Bardin <andrii.bardin@gmail.com>
2021-02-24 12:15:25 +0200

    fixed naming

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Counter counter = new Counter();
-        Thread runnable = new Thread(new CustomRunnable(counter));
+        CustomRunnable runnable = new CustomRunnable(counter);
         CustomThread myThread = new CustomThread(counter);
-        runnable.start();
+        new Thread(runnable).start();
         myThread.start();
     }
 }
