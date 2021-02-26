995e9e3b1c4dfb07b6b818d6eb469341b273b5c8
Andrii Bardin <andrii.bardin@gmail.com>
2021-02-26 09:06:30 +0200

    removed files from previous task

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Counter counter = new Counter();
-        Runnable runnable = new CustomRunnable(counter);
-        Thread myThread = new CustomThread(counter);
-        new Thread(runnable).start();
-        myThread.start();
-    }
-}
